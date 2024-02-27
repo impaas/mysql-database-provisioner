66fcbf544a9aac77598658075cfdafab5c9e400e
Ubuntu <sqlrootuser@mysql-provisioner.3qztkezsm5qurgiseirxtu23se.zx.internal.cloudapp.net>
2024-02-27 17:23:20 +0000

    Create auth

--- app.py
+++ app.py
@@ -3,10 +3,13 @@ import json
 import pymysql
 import random
 import string
+import base64
 from flask import Flask, request
 
 app = Flask(__name__)
 hostname = '20.117.188.21'
+username_required = "mysqlrootuser"
+password_required = "nthbqHOcF3tQM6K"
 
 @app.route("/resources/plans", methods=["GET"])
 def plans():
@@ -16,6 +19,24 @@ def plans():
 
 @app.route("/resources", methods=["POST"])
 def add_instance():
+
+    # Extracting password from Authorization header
+    auth_header = request.headers.get('Authorization')
+    if auth_header and auth_header.startswith('Basic '):
+        encoded_credentials = auth_header[len('Basic '):]
+        decoded_credentials = base64.b64decode(encoded_credentials).decode('utf-8')
+        username, password = decoded_credentials.split(':', 1)
+        print("\nUsername:", username)
+        print("Password:", password)
+	if username != username_required or password != password_required:
+		return "Incorrect credentials provided\n", 400
+
+
+    # Printing request body
+    print("\nRequest Body:")
+    print(request.get_data(as_text=True))
+
+
     name = request.form.get("name")
     # use the given parameters to create the instance
     conn = pymysql.connect(host=hostname,
@@ -27,7 +48,8 @@ def add_instance():
     if not name:
         return "Name not provided\n", 400
     # TODO: Error handling; check if database already exists
-    create_database_query = f"CREATE DATABASE IF NOT EXISTS {name}\n"
+    create_database_query = f"CREATE DATABASE IF NOT EXISTS {name}"
+    print(create_database_query)
     cursor.execute(create_database_query)
 
     conn.commit()
@@ -38,6 +60,16 @@ def add_instance():
 
 @app.route("/resources/<name>/bind-app", methods=["POST"])
 def bind_app(name):
+        # Extracting password from Authorization header
+    auth_header = request.headers.get('Authorization')
+    if auth_header and auth_header.startswith('Basic '):
+        encoded_credentials = auth_header[len('Basic '):]
+        decoded_credentials = base64.b64decode(encoded_credentials).decode('utf-8')
+        username, password = decoded_credentials.split(':', 1)
+        print("\nUsername:", username)
+        print("Password:", password)
+        if username != username_required or password != password_required:
+                return "Incorrect credentials provided\n", 400
     # use name and app-name to bind the service instance and the 
     # application
     app_name = request.form.get("app-name")
@@ -51,6 +83,7 @@ def bind_app(name):
 
     # create specific db user
     new_username = name + app_name
+    new_username = new_username[:31]
     new_user_password = app_name + ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
     create_user_query = f"CREATE USER '{new_username}'@'%' IDENTIFIED BY '{new_user_password}'"
     cursor.execute(create_user_query)
@@ -75,6 +108,16 @@ def bind_app(name):
 
 @app.route("/resources/<name>/bind-app", methods=["DELETE"])
 def unbind_app(name):
+        # Extracting password from Authorization header
+    auth_header = request.headers.get('Authorization')
+    if auth_header and auth_header.startswith('Basic '):
+        encoded_credentials = auth_header[len('Basic '):]
+        decoded_credentials = base64.b64decode(encoded_credentials).decode('utf-8')
+        username, password = decoded_credentials.split(':', 1)
+        print("\nUsername:", username)
+        print("Password:", password)
+        if username != username_required or password != password_required:
+                return "Incorrect credentials provided\n", 400
     app_name = request.form.get("app-name")
     if not app_name:
         return "No app-name provided\n", 400
@@ -97,6 +140,17 @@ def unbind_app(name):
 
 @app.route("/resources/<name>", methods=["DELETE"])
 def remove_instance(name):
+        # Extracting password from Authorization header
+    auth_header = request.headers.get('Authorization')
+    if auth_header and auth_header.startswith('Basic '):
+        encoded_credentials = auth_header[len('Basic '):]
+        decoded_credentials = base64.b64decode(encoded_credentials).decode('utf-8')
+        username, password = decoded_credentials.split(':', 1)
+        print("\nUsername:", username)
+        print("Password:", password)
+        if username != username_required or password != password_required:
+                return "Incorrect credentials provided\n", 400
+
     # remove the instance named "name"
     # Delete database
     conn = pymysql.connect(host=hostname, user="root", password="rootmysqltsuru")
